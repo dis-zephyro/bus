cd78f18f131d6845783d62d7d9f3c50df43ae686
dis-zephyro <zephyro@yandex.ru>
2015-07-09 14:25:51 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Мобильное меню.
+
+$(function() {
+    var pull = $('#pull');
+    menu = $('.nav');
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 719 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
+
+
+
+
 
 // Модальное окно
 
